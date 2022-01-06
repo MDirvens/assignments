93bee038746d22f427da1834522036c76d848e76
MDirvens <91317354+MDirvens@users.noreply.github.com>
2022-01-06 20:13:14 +0200

    Sagatave uzdevuma pildīšanai.

---
+++ scooterRentalService/scooterRental/scooterRental.Test/ScooterService.Tests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace scooterRental.Test
+{
+    [TestClass]
+    public class ScooterService.Test
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
